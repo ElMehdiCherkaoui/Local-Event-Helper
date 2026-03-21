69bb3b2f5db2602b3fb5269a1042505f2bc8676a
El Mehdi Cherkaoui <cherkaouim451@gmail.com>
2026-03-21 17:31:36 +0000

    feat: update App component to include routes for EventAdmin, ModerationAdmin, LogsAdmin, and OrganizerDashboard

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,26 @@ import Login from './pages/auth/Login';
 import Register from './pages/auth/Register';
 import DashboardAdmin from './pages/admin/Dashboard';
 import UserAdmin from './pages/admin/Users';
+import EventAdmin from './pages/admin/Events';
+import ModerationAdmin from './pages/admin/Moderation';
+import LogsAdmin from './pages/admin/AuditLogs';
+
+import OrganizerDashboard from './pages/organizer/Dashboard';
 
 export default function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/dashboard' element={<DashboardAdmin />} />
-        <Route path='/users' element={<UserAdmin />} />
-      </Routes>
-    </BrowserRouter>
-  )
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path='/login' element={<Login />} />
+				<Route path='/register' element={<Register />} />
+				<Route path='/admin/dashboard' element={<DashboardAdmin />} />
+				<Route path='/admin/users' element={<UserAdmin />} />
+				<Route path='/admin/events' element={<EventAdmin />} />
+				<Route path='/admin/moderation' element={<ModerationAdmin />} />
+				<Route path='/admin/logs' element={<LogsAdmin />} />
+				<Route path='/organizer/dashboard' element={<OrganizerDashboard />} />
+			</Routes>
+		</BrowserRouter>
+	)
 }
